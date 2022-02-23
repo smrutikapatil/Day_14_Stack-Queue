2d37a2a8683d3d88d592989ccaba60d14eb78aa0
smrutikapatil <96045905+smrutikapatil@users.noreply.github.com>
2022-02-23 12:52:21 +0530

    Added- Ability to create a queue of 56 -> 30 -> 70

--- com/stack_and_queue/MyStackTest.java
+++ com/stack_and_queue/MyStackTest.java
@@ -10,9 +10,9 @@ public class MyStackTest {
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(70);
 		
-		myStack.push(myFirstNode);
-		myStack.push(mySecondNode);
-		myStack.push(myThirdNode);
+		myStack.enOueue(myFirstNode);
+		myStack.enqueue(mySecondNode);
+		myStack.enoueue(myThirdNode);
 		myStack.printStack();
 	}
 }
